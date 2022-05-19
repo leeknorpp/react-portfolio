f2f5034ae03097b4f693577bf7944c17189b49a3
Lee Knorpp <leeknorpp@gmail.com>
2022-05-18 20:17:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lee Knorpp React Portfolio Application
+
---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Date from dateTime;
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Lee React Portfolio</h1>
+        <h2>Date.prototype.getDate()</h2>
+      </div>
+    );
+  }
+}
