a089c69837195ea3ab9e61c1cc7ff603e69585af
Lee Knorpp <leeknorpp@gmail.com>
2022-06-21 15:16:35 -0600

    Pre-fix

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,16 +1,50 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://leeknorpp.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems(); // calls function
   }
 
   render() {
+    const blogRecords = this.state.blogItems.map((blogItem) => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
     return (
       <div>
-        <h2>Blog</h2>
+        {
+          blogRecords //you don't have to call this.blogRecords because you made the variable inside the render statement
+        }
       </div>
     );
   }
 }
+
+export default Blog;
